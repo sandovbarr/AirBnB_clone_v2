105fb133e6dad1f7fc226bba76c709ae5ca526b6
julianramirezch <julianramirezch1@gmail.com>
2020-08-12 23:51:44 +0000

    [fix]: FileStorage engine

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,9 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow,
+    created_at = Column(DateTime, default=datetime.utcnow(),
                         nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow,
+    updated_at = Column(DateTime, default=datetime.utcnow(),
                         nullable=False)
 
     def __init__(self, *args, **kwargs):
@@ -33,6 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- models/city.py
+++ models/city.py
@@ -12,6 +12,6 @@ class City(BaseModel, Base):
     # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    #else:
+    # else:
     #    state_id = ''
     #    name = ''
