cd96a7585b3bd09aa15b7bc09fa6f6c64308539c
fernandoSandoval <sandovbarr@gmail.com>
2020-08-13 11:19:45 -0500

    [ADD] relationships and column descriptions

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-import os
+from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
@@ -14,6 +15,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship('Place',cascade="all, delete", backref='user')
     else:
         email = ''
         password = ''
