fc04df7063b70bce5ce8341a7ca19a93d920cd5c
fernandoSandoval <sandovbarr@gmail.com>
2020-08-13 17:42:56 -0500

    [FIX] new constructor

--- models/base_model.py
+++ models/base_model.py
@@ -19,21 +19,26 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if len(kwargs) != 0:
+        if kwargs:
+            print(kwargs)
             for key, value in kwargs.items():
-                if key == 'created_at':
-                    self.created_at = datetime.strptime(value,
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == 'updated_at':
-                    self.updated_at = datetime.strptime(value,
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif key != '__class__':
-                    setattr(self, key, value)
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid.uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
+        self.save()
 
     def __str__(self):
         """Returns a string representation of the instance"""
