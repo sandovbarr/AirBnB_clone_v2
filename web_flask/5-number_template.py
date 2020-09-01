43d7d5d6ccdcb57c43155c633eb7d5c705af9043
fernandoSandoval <sandovbarr@gmail.com>
2020-09-01 15:05:57 -0500

    [FIX] html conditional

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     ''' Return n if it's number '''
-    return (render_template('5-number.html', num=n))
+    return (render_template('5-number.html', n=n))
 
 
 if __name__ == '__main__':
