bef45f64af365f35a9c30d662cd5da8b6a3feaf5
fernandoSandoval <sandovbarr@gmail.com>
2020-09-01 15:40:46 -0500

    [ADD] task even odd validation

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+''' script that starts a Flask web application '''
+from flask import Flask, escape, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    ''' return for index location '''
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbtn():
+    ''' return for hbth location '''
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def r_text(text):
+    ''' show text variable'''
+    text = text.replace("_", " ")
+    return ('C %s' % escape(text))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def p_text(text='is cool'):
+    '''
+        display “Python ”,
+        followed by the value of
+        the text variable
+    '''
+    text = text.replace("_", " ")
+    return ('Python %s' % escape(text))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    ''' Return n if it's number '''
+    return ('{} is a number'.format(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    ''' Return n if it's number '''
+    return (render_template('5-number.html', num=n))
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    ''' render if it's odd or even '''
+    val = ""
+    if (n % 2 == 0):
+        val = 'even'
+    else:
+        val = 'odd'
+    return (render_template('6-number_odd_or_even.html', num=n, value=val))
+
+
+if __name__ == '__main__':
+    app.run()
