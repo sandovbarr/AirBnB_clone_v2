f8ed412641783d76d37e26f32636ed681866e8fa
Fernando <sandovbarr@gmail.com>
2020-09-02 18:12:25 -0500

    [ADD] features for states with id

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -16,15 +16,18 @@ def all_states():
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
     ''' return all states by it's id '''
-    states_id = storage.all()
+    states_id = storage.all(State)
     for k, v in states_id.items():
-        print ("{}, {}\n\n".format(k, v))
+        if id in k:
+            return (render_template('9-states.html', state_id_values=v))
+    return (render_template('9-states.html', state_id_values=None))
 
 
 @app.teardown_appcontext
 def teardown_db(error):
     ''' remove the current SQLAlchemy Session '''
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
