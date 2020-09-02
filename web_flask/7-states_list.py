8d2df4b670aa95a6a5d8b85c086b0f0ebcb2afff
fernandoSandoval <sandovbarr@gmail.com>
2020-09-01 20:30:12 -0500

    [FIX] codestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,10 +12,12 @@ def states_list():
     all_states = storage.all(State).values()
     return (render_template('7-states_list.html', states=all_states))
 
+
 @app.teardown_appcontext
 def teardown_db(error):
     ''' remove the current SQLAlchemy Session '''
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
